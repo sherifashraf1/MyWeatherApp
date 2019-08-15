0b49f02c6d4728c069e5a5e99caf08e7c15f6396
sherifashraf910 <sherifashraf910@gmail.com>
2019-08-15 08:10:47 +0200

    Add Main Tab Bar with the Countries and Profile view controllers by create lazy vars in the MainTabBarController and set them in the viewDidLoad..

---
+++ MyWeatherApp/AppModules/TabBar/MainTabBarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarViewController.swift
+//  MyWeatherApp
+//
+//  Created by Sherif on 8/15/19.
+//  Copyright © 2019 Sherif. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    lazy var countriesList: UINavigationController = {
+        let vc = CountriesListViewController(nibName: "CountriesListViewController", bundle: nil)
+        vc.tabBarItem.title = "Countries"
+        vc.tabBarItem.image = #imageLiteral(resourceName: "earth")
+        return UINavigationController(rootViewController: vc)
+    }()
+    
+    lazy var profile: UINavigationController = {
+        let vc = UIViewController()
+        vc.tabBarItem.title = "Profile"
+        vc.tabBarItem.image = #imageLiteral(resourceName: "profile")
+        return UINavigationController(rootViewController: vc)
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setViewControllers([countriesList , profile], animated: true)
+
+    }
+
+}
